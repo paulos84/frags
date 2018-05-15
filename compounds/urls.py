40b45c94c2bc262aa8adb596786506b23cf1818f
Paul Davis <pjdavis@gmx.com>
2018-05-16 00:35:08 +0200

    edit urlconfs

--- compounds/urls.py
+++ compounds/urls.py
@@ -3,7 +3,6 @@ from compounds.views.compound_list_view import CompoundListView
 
 urlpatterns = [
     re_path(r'^$', CompoundListView.as_view(), name='index'),
-    re_path(r'^compounds/$', CompoundListView.as_view(), name='index'),
     # re_path(r'^books/$', views.BookListView.as_view(), name='books'),
     # re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
 ]

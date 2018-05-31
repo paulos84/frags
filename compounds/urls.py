1df8e52d1aa9232949bcab4408a157247ba3cc4d
Paul Davis <pjdavis@gmx.com>
2018-05-31 23:52:35 +0200

    update urls

--- compounds/urls.py
+++ compounds/urls.py
@@ -2,10 +2,9 @@ from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from compounds.views.compound_list import CompoundListView
-from compounds.views.compound_detail import CompoundDetailView
-from compounds.views.compound_create import CompoundCreateView, process_cas
-from compounds.views.compound_update import CompoundUpdateView
+from compounds.views import CompoundListView, CompoundCreateView, CompoundDetailView, CompoundUpdateView
+from compounds.views.compound_create import process_cas
+
 
 urlpatterns = [
     re_path(r'^$', CompoundListView.as_view(), name='index'),
@@ -13,5 +12,4 @@ urlpatterns = [
     path('compound/add/', CompoundCreateView.as_view(), name='compound-add'),
     re_path(r'^compound/update/(?P<pk>\d+)$', CompoundUpdateView.as_view(), name='compound-update'),
     path('ajax/process_cas/', process_cas, name='process_cas'),
-    # # re_path(r'^book/(?P<pk>\d+)$', views.CompoundDetailView.as_view(), name='book-detail'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- compounds/views/__init__.py
+++ compounds/views/__init__.py
@@ -1,11 +1,11 @@
 from .compound_create import CompoundCreateView
 from .compound_detail import CompoundDetailView
 from .compound_list import CompoundListView
-from .compound_update import CompoundUpdateForm
+from .compound_update import CompoundUpdateView
 
 __all__ = [
     CompoundCreateView,
     CompoundDetailView,
     CompoundListView,
-    CompoundUpdateForm,
+    CompoundUpdateView,
     ]

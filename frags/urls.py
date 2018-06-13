ee4c1e00c415e34bcef0c8a9fb4ef2095291a8a5
Paul Davis <pjdavis@gmx.com>
2018-06-13 22:15:02 +0200

    update template styling

--- frags/urls.py
+++ frags/urls.py
@@ -4,14 +4,15 @@ from django.conf.urls import include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(), name='login'),
-    re_path(r'^compounds/', include('compounds.urls')),
-    re_path(r'^api/', include('api.urls')),
-    re_path(r'^$', RedirectView.as_view(url='/compounds/', permanent=True)),
+    path('logout', LogoutView.as_view(), {'next_page': '/'}, name='logout'),
+    re_path(r'compounds/', include('compounds.urls')),
+    re_path(r'api/', include('api.urls')),
+    re_path(r'', RedirectView.as_view(url='/compounds/', permanent=True)),
 ]
 
 # Use static() to add url mapping to serve static files during development only
@@ -20,5 +21,5 @@ urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
-        path(r'^debug', include(debug_toolbar.urls)),
+        path(r'debug', include(debug_toolbar.urls)),
     ] + urlpatterns

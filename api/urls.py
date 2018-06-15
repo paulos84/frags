3fa181082707c9b873bc42b6a91af6f82bd01090
Paul Davis <pjdavis@gmx.com>
2018-06-16 00:10:38 +0200

    move loginform to top nav bar

--- api/urls.py
+++ api/urls.py
@@ -5,11 +5,11 @@ from .views import AllCompoundsData, schema_view
 urlpatterns = [
     # url(r'^site-data/(?P<code>\w+)/$', views.AllSiteData.as_view()),
     # url(r'^site-data/(?P<code>\w+)/(?P<days>[0-9]+)/$', views.RecentSiteData.as_view()),
-    path('compounds/all/', AllCompoundsData.as_view()),
+    path('compounds/all/', AllCompoundsData.as_view(), name='api_all'),
     # url(r'^data/(?P<date1>\d{4}-\d{2}-\d{2})/$', views.DateFilterData.as_view()),
     # url(r'^data/(?P<date1>\d{4}-\d{2}-\d{2})/(?P<date2>\d{4}-\d{2}-\d{2})/$', views.DateFilterData.as_view()),
     # url(r'^order-pie$', views.order_pie),
-    path('docs/', schema_view)
+    path('docs/', schema_view, name='api_docs'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)

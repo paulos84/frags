4309d9da50d6a440e76197063a3f1b7a383badff
Paul Davis <pjdavis@gmx.com>
2018-12-02 16:03:21 +0000

    update with datatables logic

--- static/js/home.js
+++ static/js/home.js
@@ -8,10 +8,40 @@ $(document).ready(function() {
       // opening the one you clicked on
       $(this).parent().addClass('open');
   });
-      $('.mech_parent').hover( function(event){
-          event.preventDefault();
-          event.stopPropagation();
-          $('.dropdown-submenu a.mech_parent').parent().removeClass('open');
-          $(this).parent().addClass('open');
-      });
+  $('.mech_parent').hover( function(event){
+      event.preventDefault();
+      event.stopPropagation();
+      $('.dropdown-submenu a.mech_parent').parent().removeClass('open');
+      $(this).parent().addClass('open');
+  });
+  $('#development_table').dataTable({
+        paging: true,
+        bAutoWidth: false ,
+        aoColumns : [
+            { sWidth: '5%' }, 
+			{ sWidth: '5%' }, 
+			{ sWidth: '12%' }, 
+			{ sWidth: '18%' },
+			{ sWidth: '12%' },
+            { sWidth: '6%' },
+			{ sWidth: '42%' }
+        ],
+        dom: 'Bfrtip',
+        buttons: [
+            {
+			 text: 'Show current trials',
+			 action: function () {
+				$('.study-col').toggle();
+				$('.dt-buttons').children().first().text(function(i, text){
+					  return text === "Hide current trials" ? "Show current trials" : "Hide current trials";
+				  });
+				}
+			},
+            {
+			 extend: 'collection',
+             text: 'Select company',
+             buttons: companies
+			 },
+        ]
+    });
 });
